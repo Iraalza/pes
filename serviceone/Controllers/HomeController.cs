ebd8def4320616e93dcebd9510d1a1e02d034b39
Ira <43280192+Iraalza@users.noreply.github.com>
2022-12-19 21:43:42 +0300

    first commit

---
+++ serviceone/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace serviceone.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    private HttpClient _client;
+
+    public HomeController()
+    {
+        _client = new HttpClient();
+        _client.BaseAddress = new Uri("http://servicetwo:80/");
+        _client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    [HttpGet("test")]
+    public IActionResult Test()
+    {
+        return Ok("Single works!");
+    }
+    
+    
+    [HttpGet("network")]
+    public IActionResult NetworkRequest()
+    {
+        var response = _client.GetAsync("WeatherForecast").Result;
+        return Ok(response.Content.ReadAsStringAsync().Result);
+    }
+}
+
